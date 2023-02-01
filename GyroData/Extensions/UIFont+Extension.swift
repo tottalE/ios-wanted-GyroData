7d0c0693d88ff11adabaefcc7491b924467ad566
jin <pearlisrich@naver.com>
2023-02-01 19:26:33 +0900

    feat: Font 설정

---
+++ GyroData/Extensions/UIFont+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  UIFont+Extension.swift
+//  GyroData
+//
+//  Created by jin on 1/30/23.
+//
+
+import UIKit
+
+extension UIFont {
+    var customBodyFont: UIFont {
+        return UIFont(name: "establishRoomNo703OTF", size: 15) ?? UIFont()
+    }
+    
+    var customTitleFont: UIFont {
+        return UIFont(name: "establishRoomNo703OTF", size: 32) ?? UIFont()
+    }
+    
+    var customLargeTitleFont: UIFont {
+        return UIFont(name: "establishRoomNo703OTF", size: 42) ?? UIFont()
+    }
+}
