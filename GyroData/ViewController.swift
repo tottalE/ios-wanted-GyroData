36eee544c528f81d067c81e28d8ee2228d4df921
jin <pearlisrich@naver.com>
2023-02-01 21:53:34 +0900

    feat: NavigationBar, NavigationItem Settings
    - 커스텀 폰트 적용
    - NavigationBar, NavigationItem에 해당하는 Constant 네임 스페이스 처리

--- GyroData/ViewController.swift
+++ GyroData/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private enum Constant {
+        static let navigationTitle = "목록"
+        static let navigationRightButtonTitle = "측정"
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        configureNavigationItem()
+        configureNavigationBar()
     }
 
-
+    private func configureNavigationBar() {
+        navigationController?.navigationBar.topItem?.title = Constant.navigationTitle
+        navigationController?.navigationBar.titleTextAttributes = [.font: UIFont().customTitleFont]
+    }
+    
+    private func configureNavigationItem() {
+        let rightButtonItem = UIBarButtonItem(
+            title: Constant.navigationRightButtonTitle,
+            style: .plain,
+            target: self,
+            action: nil
+        )
+        rightButtonItem.setTitleTextAttributes(
+            [.font: UIFont().customTitleFont],
+            for: .normal
+        )
+        navigationItem.rightBarButtonItem = rightButtonItem
+    }
 }
 
